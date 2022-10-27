e9b808d1ae43c75782607e20f2ad51c29eb37864
Andrew Cook <andrewhcook1@gmail.com>
2022-10-26 23:25:43 -0500

    feat: rudimentary nav-bar and changes through end of 10-26-22

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route, Switch, Link} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import {Posts, Register, LogIn, Logout, Home} from './Components/index';
 import { fetchPosts } from './api/Requests.js';
 
@@ -25,8 +25,41 @@ const App = () => {
   }, [])
   // make the nav bar then make the rest of the UI.
   // use router to show the appropriate view
+  // NAV BAR on top >> login component shows below nav bar upon first load >> posts in center below nav bar and home component>>
+  // Home message 
   // vvvv THIS IS A PLACEHOLDER TO ENSURE INDIVIDUAL COMPONENTS DISPLAY PROPERLY vvvv//
-  return <> <inline><Logout setToken= {setToken}></Logout><Register setToken={setToken}></Register><LogIn setToken={setToken}></LogIn></inline><Posts posts={posts}> </Posts></>
+
+    //return <> <inline><Logout setToken= {setToken}></Logout><Register setToken={setToken}></Register><LogIn setToken={setToken}></LogIn></inline><Posts posts={posts}> </Posts></>
+    // ^^^ delete after line below is complete
+    return(
+      <BrowserRouter>
+      <div id = "container">
+        <div id = "nav-bar">
+          <Link to ="/Home">Home</Link>
+          <Link to = "/Posts">Posts</Link>
+          <Link to = "/LogIn">Login</Link>
+
+
+        </div>
+      <div id = "main-section">
+      <Route path = "/Login">
+        <LogIn setToken = {setToken}></LogIn>
+        <div>Not a user? Register Below: </div>
+        <Register setToken={setToken}></Register>
+        </Route>
+        <Route path = "/Posts">
+          <Posts posts= {posts}></Posts>
+        </Route>
+        <Route path = "/Home">
+          { token ? <>
+          <Logout setToken={setToken}></Logout>
+          <Home></Home>
+          <div>Placeholder for createPosts</div> </>: null}
+          <Posts posts = {posts}></Posts>
+        </Route>
+    </div>
+    </div>
+    </BrowserRouter>)
 };
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
